89ae6261cc3ba396ea6742ea5c9bfc863ecd3a75
Nurbek Avtandilov <avtandilov06@gmail.com>
2025-07-20 19:01:35 +0600

    add changes

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -34,11 +34,6 @@ export const Checkout = ({ cartItems, setCartItems, partner, onBack }) => {
     const total = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
     const handleSubmit = () => {
-        if (!address.trim() || !phone.trim()) {
-            alert("Пожалуйста, заполните адрес и телефон.");
-            return;
-        }
-
         const foods = cartItems.map((item) => ({ food_id: item._id, count: item.quantity }));
 
         const orderData = {
@@ -79,7 +74,11 @@ export const Checkout = ({ cartItems, setCartItems, partner, onBack }) => {
     }, [cartItems, address, phone, comment, deliveryType]);
 
     const isValid = useMemo(() => {
-        return cartItems.length && address && phone && deliveryType;
+        if (deliveryType === 'delivery') {
+            return cartItems.length && phone && deliveryType && address;
+        }
+
+        return cartItems.length && phone && deliveryType;
     }, [cartItems, address, phone, deliveryType]);
 
     useEffect(() => {
@@ -101,9 +100,17 @@ export const Checkout = ({ cartItems, setCartItems, partner, onBack }) => {
                     <div key={item._id} className="checkout-item">
                         <p>{item.name}</p>
                         <div className="increase-decrease-buttons">
-                            <button onClick={() => updateQuantity(item._id, item.quantity - 1)}>-</button>
+                            <button onClick={() => updateQuantity(item._id, item.quantity - 1)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
+                                </svg>
+                            </button>
                             <span>{item.quantity}</span>
-                            <button onClick={() => updateQuantity(item._id, item.quantity + 1)}>+</button>
+                            <button onClick={() => updateQuantity(item._id, item.quantity + 1)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" />
+                                </svg>
+                            </button>
                         </div>
                     </div>
                 ))}
