58e9becb4b9a08919c4f531bb3710159ec5d02e5
Nurbek Avtandilov <avtandilov06@gmail.com>
2025-08-17 00:56:53 +0600

    add changes

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -206,12 +206,21 @@ export const Checkout = ({ cartItems, setCartItems, partner, onBack }) => {
             <div className="form-wrapper">
                 <div className="field-wrapper select-wrapper">
                     <label htmlFor="delivery-type" className="field-label">Тип заказа</label>
-                    <select id="delivery-type" className="select-field" value={deliveryType} onChange={(e) => setDeliveryType(e.target.value)}>
-                        <option value="" disabled>Выбрать тип заказа</option>
+                    <select 
+                        id="delivery-type" 
+                        className={`select-field ${!deliveryType ? 'select-placeholder' : ''}`} 
+                        value={deliveryType} 
+                        onChange={(e) => setDeliveryType(e.target.value)}
+                    >
+                        <option value="" disabled>Выберите тип доставки</option>
                         {partner?.delivery_options?.map((option) => (
                             <option key={option} value={option}>{OPTIONS_LABEL[option]}</option>
                         ))}
                     </select>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                        <path d="M297.4 470.6C309.9 483.1 330.2 483.1 342.7 470.6L534.7 278.6C547.2 266.1 547.2 245.8 534.7 233.3C522.2 220.8 501.9 220.8 489.4 233.3L320 402.7L150.6 233.4C138.1 220.9 117.8 220.9 105.3 233.4C92.8 245.9 92.8 266.2 105.3 278.7L297.3 470.7z"/>
+                    </svg>
                 </div>
 
                 {deliveryType === 'delivery' && (
@@ -223,7 +232,7 @@ export const Checkout = ({ cartItems, setCartItems, partner, onBack }) => {
 
                 <div className="field-wrapper">
                     <label htmlFor="name" className="field-label">Имя</label>
-                    <input type="text" id="name" className="text-field" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input type="text" id="name" className="text-field" placeholder="Руслан" required value={username} onChange={(e) => setUsername(e.target.value)} />
                 </div>
 
                 <div className="field-wrapper">
@@ -235,6 +244,7 @@ export const Checkout = ({ cartItems, setCartItems, partner, onBack }) => {
                         id="phone"
                         className="text-field"
                         placeholder="0555 555 555"
+                        required
                         value={phone}
                         onChange={(e) => setPhone(e.target.value)}
                         maxLength={10}
