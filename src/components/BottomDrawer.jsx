27a19397a86ba4f86463e6f6d7ab92debb6a7d09
Alisher <zumanovaliser814@gmail.com>
2025-08-28 14:48:59 +0600

    react to 19, other packages update

--- src/components/BottomDrawer.jsx
+++ src/components/BottomDrawer.jsx
@@ -1,23 +1,26 @@
-import React, { useEffect } from 'react';
-import { motion, AnimatePresence, useMotionValue } from 'framer-motion';
-import './Drawer.css';
+import React, { useEffect } from "react";
+import { motion, AnimatePresence, useMotionValue } from "framer-motion";
+import "./Drawer.css";
 
 export function BottomDrawer({ isOpen, onClose, children }) {
   const y = useMotionValue(0);
 
   // Close if dragged down past threshold
   const handleDragEnd = (_, info) => {
-    const shouldClose = info.velocity.y > 500 || info.point.y > window.innerHeight * 0.4;
+    const shouldClose =
+      info.velocity.y > 500 || info.point.y > window.innerHeight * 0.4;
     if (shouldClose) {
-        // handleSelect([]);
-        onClose();
-    };
+      // handleSelect([]);
+      onClose();
+    }
   };
 
   // Prevent background scroll when open
   useEffect(() => {
-    document.body.style.overflow = isOpen ? 'hidden' : '';
-    return () => { document.body.style.overflow = ''; };
+    document.body.style.overflow = isOpen ? "hidden" : "";
+    return () => {
+      document.body.style.overflow = "";
+    };
   }, [isOpen]);
 
   return (
@@ -31,8 +34,8 @@ export function BottomDrawer({ isOpen, onClose, children }) {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             onClick={() => {
-                // handleSelect([]);
-                onClose();
+              // handleSelect([]);
+              onClose();
             }}
           />
 
@@ -40,22 +43,20 @@ export function BottomDrawer({ isOpen, onClose, children }) {
           <motion.div
             className="drawer-container"
             style={{ y }}
-            initial={{ y: '100%' }}
+            initial={{ y: "100%" }}
             animate={{ y: 0 }}
-            exit={{ y: '100%' }}
-            transition={{ type: 'tween', duration: 0.3 }}
+            exit={{ y: "100%" }}
+            transition={{ type: "tween", duration: 0.3 }}
             drag="y"
             dragDirectionLock
             dragConstraints={{ top: 0, bottom: 0 }}
             onDragEnd={handleDragEnd}
             // onClick={(e) => {e.stopPropagation()}}
           >
-            <div className='drawer-line'>
+            <div className="drawer-line">
               <div className="drawer-handle" />
             </div>
-            <div className="drawer-content">
-              {children}
-            </div>
+            <div className="drawer-content">{children}</div>
           </motion.div>
         </>
       )}
